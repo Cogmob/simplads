5a48b517cbc843f87c9c7acdeffbbdb932ba94fb
cogmob <luke.avery@live.co.uk>
2016-09-22 15:16:17 +0100

    added readme to setup

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
         name='simplads',
-        version='0.22',
+        version='0.23',
         author='Luke Avery',
+        author_email='cogmob@gmail.com',
+        url='https://github.com/Cogmob/simplads',
         description=('A set of tools for conveniently using monads'),
         packages=[
             'simplads',
@@ -12,5 +14,6 @@ setup(
             'simplads.simplad_monad',
             'simplads.simplad_monad.namedtuples',
             'simplads.simplad_bundle'
-        ]
+        ],
+        long_description=open('README.rst', 'r').read()
 )
